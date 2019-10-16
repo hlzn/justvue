b57aaca19a27a3555ccc4759b2f4735ab6166268
Hugo Lozano <hugo@hlzn.net>
2019-10-16 11:56:42 -0500

    ok

--- vuedemo/src/componentes/Inicio.vue
+++ vuedemo/src/componentes/Inicio.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>
-        <h3>Inicio</h3>
+    <div class="container">
+        <app-header></app-header>
+        <div class="row">
+            <h3>Inicio</h3>
+        </div>
     </div>
 </template>
 
 <script>
+import Header from './Header'
+
 export default {
-    
+    components:{
+        appHeader: Header
+    }    
 }
 </script>
--- vuedemo/src/main.js
+++ vuedemo/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
 
 import {routes} from './routes';
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const router = new VueRouter({
   mode: 'history',
